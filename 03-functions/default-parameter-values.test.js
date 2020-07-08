2dc0b8af7ca53dbc3535841b1e14184646cbbf76
Marc Altmann <marcaltmann@posteo.de>
2020-07-08 21:58:13 +0200

    Remove one test

--- 03-functions/default-parameter-values.test.js
+++ 03-functions/default-parameter-values.test.js
@@ -1,8 +1,19 @@
-test('basic default values', () => {
-  function sum(x, y = 0) {
-    return x + y;
+test('undefined also triggers default', () => {
+  function doSomething(name, position = 'default') {
+    return position;
   }
 
-  expect(sum(5, 4)).toBe(9);
-  expect(sum(5)).toBe(5);
+  expect(doSomething('Schmidt', 'superior')).toBe('superior');
+  expect(doSomething('Schmidt', null)).toBe(null);
+  expect(doSomething('Schmidt')).toBe('default');
+  expect(doSomething('Schmidt', undefined)).toBe('default');
+});
+
+test('any position is possible', () => {
+  function returnName(name = 'Dirk', age) {
+    return name;
+  }
+
+  expect(returnName('Mark', 30)).toBe('Mark');
+  expect(returnName(undefined, 50)).toBe('Dirk');
 });
