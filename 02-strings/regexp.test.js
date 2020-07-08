73260b0a17383a4202f618eb7a383545191cfdd5
Marc Altmann <marcaltmann@posteo.de>
2020-07-08 13:00:11 +0200

    Add sticky flag test

--- 02-strings/regexp.test.js
+++ 02-strings/regexp.test.js
@@ -1,3 +1,25 @@
+test('sticky flag', () => {
+  // The sticky flag is like a dynamic ^ matcher...
+  let text = 'word1 word2 word3';
+  let regexp = /word/y;
+
+  expect(regexp.sticky).toBe(true);
+  expect(regexp.lastIndex).toBe(0);
+
+  let result = regexp.exec(text);
+
+  expect(typeof result).toBe('object');
+  expect(result.length).toBe(1);
+  expect(result[0]).toBe('word');
+  expect(regexp.lastIndex).toBe(4);
+
+  // Does not match again, because last index is in the position of the space
+  // before 'word2'.
+  result = regexp.exec(text);
+  expect(result).toBeNull();
+  expect(regexp.lastIndex).toBe(0);
+});
+
 test('duplicating regexps', () => {
   let re1 = /^.$/;
   let re2 = new RegExp(re1, 'g');
@@ -13,4 +35,7 @@ test('regexp object properties', () => {
 
   expect(re1.source).toEqual('^.$');
   expect(re1.flags).toEqual('gu');
+
+  expect(re1.global).toBe(true);
+  expect(re1.unicode).toBe(true);
 });
