d5c813547d93b71f5d3db61410b8adc0d9197ae0
Marc Altmann <marcaltmann@posteo.de>
2020-07-09 16:18:09 +0200

    Add spread operator test

--- 03-functions/rest-spread.test.js
+++ 03-functions/rest-spread.test.js
@@ -15,8 +15,20 @@ test('rest parameter', () => {
   expect(add(1, 2, 3, 4, 5)).toBe(15);
 });
 
-test('also work with the Function constructor', () => {
+test('rest also works with the Function constructor', () => {
   let pickFirst = new Function('...args', 'return args[0];');
 
   expect(pickFirst(3, 2, 1)).toBe(3);
+});
+
+test('spread operator', () => {
+  let numbers = [5, -3, 12, 200];
+
+  let max1 = Math.max.apply(Math, numbers);
+  let max2 = Math.max(...numbers);
+  let max3 = Math.max(...numbers, 250);
+
+  expect(max1).toBe(200);
+  expect(max2).toBe(200);
+  expect(max3).toBe(250);
 });
