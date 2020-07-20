8755d9c32219d4eb4bacbb3a4784100b3b1dc824
Marc Altmann <marcaltmann@posteo.de>
2020-07-20 22:31:41 +0200

    Add tests for sets

--- 07-sets-maps/sets.test.js
+++ 07-sets-maps/sets.test.js
@@ -13,3 +13,24 @@ test('basic usage', () => {
 
   expect(numbers.size).toBe(0);
 });
+
+test('forEach() method', () => {
+  let set = new Set([1, 2, 3]);
+
+  expect.assertions(6);
+
+  set.forEach((arg1, arg2, s) => {
+    expect(arg1).toBe(arg2);
+    expect(s).toBe(set);
+  });
+});
+
+test('weak set basics', () => {
+  let set = new WeakSet();
+
+  expect(() => {
+    set.add(5);
+  }).toThrow(TypeError);
+
+  expect(set.size).toBeUndefined();
+})
