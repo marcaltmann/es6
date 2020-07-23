6234369bdf84b5eb5faf91d5738847a99a9510de
Marc Altmann <marcaltmann@posteo.de>
2020-07-23 22:29:10 +0200

    Improve simple async task runner test

--- 08-iterators-generators/asynchronous.test.js
+++ 08-iterators-generators/asynchronous.test.js
@@ -1,23 +1,18 @@
 test('simple task runner', () => {
-  expect.assertions(3);
+  let savedResults = new Array();
 
   function run(taskDef) {
     let task = taskDef();
 
     let result = task.next();
-
-    expect(result).toEqual({
-      done: false,
-      value: 1,
-    })
+    savedResults.push(result);
 
     step();
 
     function step() {
       if (!result.done) {
         result = task.next(result.value);
-
-        expect(typeof result).toBe('object');
+        savedResults.push(result);
 
         step();
       }
@@ -28,4 +23,9 @@ test('simple task runner', () => {
     let a = yield 1;
     yield a + 2;
   });
+
+  expect(savedResults.length).toBe(3);
+  expect(savedResults[0].value).toBe(1);
+  expect(savedResults[1].value).toBe(3);
+  expect(savedResults[2].value).toBeUndefined();
 });
