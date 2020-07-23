4b7e5a34bd301ef07031a6688404ae13d041b282
Marc Altmann <marcaltmann@posteo.de>
2020-07-23 11:23:21 +0200

    Add more advanced tests

--- 08-iterators-generators/advanced.test.js
+++ 08-iterators-generators/advanced.test.js
@@ -10,3 +10,61 @@ test('yield expressions and next arguments', () => {
   expect(iterator.next(1).value).toBe(3);
   expect(iterator.next(10).value).toBe(20);
 });
+
+test('return statements inside generators', () => {
+  function *numbersGen() {
+    yield 1;
+    return 10;
+    yield 2;
+    yield 3;
+  }
+  let numbers = numbersGen();
+
+  expect(numbers.next()).toEqual({
+    done: false,
+    value: 1,
+  });
+  expect(numbers.next()).toEqual({
+    done: true,
+    value: 10,
+  });
+  expect(numbers.next()).toEqual({
+    done: true,
+    value: undefined,
+  });
+});
+
+test('throw() function without the generator catching', () => {
+  function *numbersGen() {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+  let numbers = numbersGen();
+
+  expect(numbers.next().value).toBe(1);
+  expect(() => {
+    numbers.throw(new Error('Boom'));
+  }).toThrow('Boom');
+});
+
+test('throw() function with catch clause within generator', () => {
+  function *numbersGen() {
+    try {
+      yield 1;
+      yield 2;
+      yield 3;
+    } catch (e) {
+      yield -1;
+      yield -2;
+      yield -3;
+    }
+  }
+  let numbers = numbersGen();
+
+  expect(numbers.next().value).toBe(1);
+  expect(numbers.throw(new Error('Boom')).value).toBe(-1);
+  expect(numbers.next().value).toBe(-2);
+  expect(numbers.next().value).toBe(-3);
+  expect(numbers.next().value).toBe(undefined);
+});
