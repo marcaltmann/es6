612b2d63a4d8a4693718cf7b7e6ecdcaccff0ae4
Marc Altmann <marcaltmann@posteo.de>
2020-07-24 13:07:17 +0200

    Add final async task runner test

--- 08-iterators-generators/asynchronous.test.js
+++ 08-iterators-generators/asynchronous.test.js
@@ -72,3 +72,49 @@ test('asynchronous task runner', done => {
     done();
   });
 });
+
+test('asynchronous task runner with exception', done => {
+  function fetchData(delay) {
+    return function(callback) {
+      setTimeout(function() {
+        callback(new Error('Boom'), null);
+      }, delay);
+    };
+  }
+
+  function run(taskDef) {
+    let task = taskDef();
+
+    let result = task.next();
+
+    step();
+
+    function step() {
+      if (!result.done) {
+        if (typeof result.value === 'function') {
+          result.value(function(err, data) {
+            if (err) {
+              result = task.throw(err);
+              return;
+            }
+
+            result = task.next(data);
+            step();
+          })
+        } else {
+          result = task.next(result.value);
+          step();
+        }
+      }
+    }
+  }
+
+  run(function *() {
+    try {
+      let result = yield fetchData(50);
+    } catch (e) {
+      expect(e.message).toBe('Boom');
+    }
+    done();
+  });
+});
