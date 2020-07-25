09c750bd7a71e8fb69005b44612315b95d333414
Marc Altmann <marcaltmann@posteo.de>
2020-07-25 22:19:28 +0200

    First tests for classes

---
+++ 09-classes/basics.test.js
@@ -0,0 +1,37 @@
+test('a simple class', () => {
+  class Person {
+    constructor(name) {
+      this.name = name;
+    }
+
+    getName() {
+      return this.name;
+    }
+  }
+
+  let p1 = new Person('Roger O. Thornton');
+
+  expect(p1.getName()).toBe('Roger O. Thornton');
+  expect(p1 instanceof Person).toBeTruthy();
+  expect(p1 instanceof Object).toBeTruthy();
+  expect(Person.name).toBe('Person');
+  expect(typeof Person).toBe('function');
+  expect(typeof Person.prototype.getName).toBe('function');
+});
+
+test('class expressions', () => {
+  let Person = class {
+    constructor(name) {
+      this.name = name;
+    }
+
+    getName() {
+      return this.name;
+    }
+  };
+
+  let p1 = new Person('Roger O. Thornton');
+
+  expect(p1.getName()).toBe('Roger O. Thornton');
+  expect(Person.name).toBe('Person');  // Different from the book.
+});
