f1f4e6abd7678040dca0c54b207717994dca00a1
Marc Altmann <marcaltmann@posteo.de>
2020-08-05 21:41:57 +0200

    Add more chaining tests

--- 11-promises/chaining.test.js
+++ 11-promises/chaining.test.js
@@ -12,18 +12,48 @@ test('simple chaining', (done) => {
 
   let p1 = Promise.resolve(42);
 
-  let p2 = p1.then(
-    (result) => {
-      return result + 1;
-    }
-  );
+  let p2 = p1.then((result) => {
+    return result + 1;
+  });
 
-  p2.then(
-    (result) => {
-      expect(result).toBe(43);
-      done();
-    }
-  );
+  p2.then((result) => {
+    expect(result).toBe(43);
+    done();
+  });
 
   expect(p2).toBeInstanceOf(Promise);
+});
+
+test('with an error', (done) => {
+  let p1 = Promise.resolve(42);
+
+  let p2 = p1.then((result) => {
+    throw new Error('Boom!');
+  });
+
+  p2.catch((error) => {
+    expect(error.message).toBe('Boom!');
+    done();
+  });
+
+  expect(p2).toBeInstanceOf(Promise);
+});
+
+test('returning a promise', (done) => {
+  expect.assertions(2);
+
+  let p1 = Promise.reject('Boom!');
+
+  let p2 = Promise.resolve(42);
+
+  let p3 = p1.catch((reason) => {
+    return p2;
+  });
+
+  p3.then((result) => {
+    expect(result).toBe(42);
+    done();
+  });
+
+  expect(p3).not.toBe(p2);
 });
